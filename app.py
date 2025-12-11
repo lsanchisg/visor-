868b61a50f4c2a7c4a33c750ee968d92e181f44a
lsanchisg <lsanchisg@gmail.com>
2025-12-11 21:52:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 
 # --- DEBUG: Show me all files in the current folder ---
 import os
-st.write("📂 **Files found in this folder:**")
-st.write(os.listdir('.')) # This prints the list of files to the screen
-st.write("---")
 
 
 
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
+
+st.write("📂 **Files found in this folder:**")
+st.write(os.listdir('.')) # This prints the list of files to the screen
+st.write("---")
+
 
 # Try to import Plotly
 try:
@@ -252,4 +253,5 @@ else:
     
     *Please ensure the file matches this naming pattern exactly and is in the same folder.*
     """)
+
 
